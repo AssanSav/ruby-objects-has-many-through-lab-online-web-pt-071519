afc9505fc77f1a1a2fd5f17540822fce56d254f9
assane savadogo <assane.savadogo81@gmail.com>
2019-08-29 23:49:25 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Appointment
+  # attr_accessor :name
+  # @@all = []
+  
+  # def initialize(name)
+  #   @name = name 
+  #   @@all << self 
+  # end
+  
+  # def self.all 
+  #   @@all 
+  end
+end
