d41f578e54582dbbcdf4e9a43df0ca04f6c654cc
assane savadogo <assane.savadogo81@gmail.com>
2019-08-29 23:31:29 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -4,10 +4,10 @@ class Genre
   # attr_accessor :name
   # @@all = []
   
-  # def initialize(name)
-  #   @name = name 
-  #   @@all << self
-  # end
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
   
   # def self.all 
   #   @@all 
