c75970bd99e8aef238b73c45427def3d3d0973b8
assane savadogo <assane.savadogo81@gmail.com>
2019-08-29 23:51:28 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,15 +1,17 @@
 require 'pry'
 
 class Appointment
-  # attr_accessor :name
-  # @@all = []
+   attr_accessor :date, :patient, :doctor
+   @@all = []
   
-  def initialize(name)
-    @name = name 
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
     @@all << self 
   end
   
-  # def self.all 
-  #   @@all 
-  #end
+  def self.all 
+    @@all 
+  end
 end
